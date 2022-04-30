309c24b2fcbcf5baf6fb1d2cfc9ddf04b543141e
tsor13 <tsor1313@gmail.com>
2022-04-30 13:31:58 -0600

    support for all gpt3 models

--- lmtools/lm_gpt3.py
+++ lmtools/lm_gpt3.py
@@ -8,7 +8,7 @@ from lmtools.lmsampler_baseclass import LMSamplerBaseClass
 class LM_GPT3(LMSamplerBaseClass):
     def __init__(self, model_name):
         """
-        Supported models: 'ada', 'babbage', 'curie', 'davinci', 'gpt3-ada', gpt3-babbage', gpt3-curie', gpt3-davinci', 'text-davinci-002'
+        Supported models: 'ada', 'babbage', 'curie', 'davinci', 'gpt3-ada', gpt3-babbage', gpt3-curie', gpt3-davinci', 'text-ada-001', 'text-babbage-001', 'text-curie-001', 'text-davinci-002'
         """
         super().__init__(model_name)
         if "gpt3" in model_name:
@@ -17,9 +17,9 @@ class LM_GPT3(LMSamplerBaseClass):
         else:
             self.engine = self.model_name
         # make sure engine is a valid model
-        if self.engine not in ["ada", "babbage", "curie", "davinci", "text-davinci-002"]:
+        if self.engine not in ["ada", "babbage", "curie", "davinci", "text-ada-001", "text-babbage-001", "text-curie-001", "text-davinci-002"]:
             raise ValueError(
-                "Invalid model name. Must be one of: 'ada', 'babbage', 'curie', 'davinci'", "text-davinci-002"
+                "Invalid model name. Must be one of: 'ada', 'babbage', 'curie', 'davinci', 'text-ada-001', 'text-babbage-001', 'text-curie-001', 'text-davinci-002'"
             )
         # make sure API key is set
         if openai.api_key is None:
